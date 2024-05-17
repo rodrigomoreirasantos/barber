eb8db00bf83ec2d853e720079bfbd3b47afacd0f
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-05-17 15:33:02 -0400

    feat: change text from home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
-import { ptBR } from "date-fns/locale";
+import { enCA } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
@@ -11,28 +11,29 @@ import { authOptions } from "../_lib/auth";
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  const [barbershops, recommendedBarbershops, confirmedBookings] = await Promise.all([
-    db.barbershop.findMany({}),
-    db.barbershop.findMany({
-      orderBy: {
-        id: "asc",
-      },
-    }),
-    session?.user
-      ? db.booking.findMany({
-          where: {
-            userId: (session.user as any).id,
-            date: {
-              gte: new Date(),
+  const [barbershops, recommendedBarbershops, confirmedBookings] =
+    await Promise.all([
+      db.barbershop.findMany({}),
+      db.barbershop.findMany({
+        orderBy: {
+          id: "asc",
+        },
+      }),
+      session?.user
+        ? db.booking.findMany({
+            where: {
+              userId: (session.user as any).id,
+              date: {
+                gte: new Date(),
+              },
             },
-          },
-          include: {
-            service: true,
-            barbershop: true,
-          },
-        })
-      : Promise.resolve([]),
-  ]);
+            include: {
+              service: true,
+              barbershop: true,
+            },
+          })
+        : Promise.resolve([]),
+    ]);
 
   return (
     <div>
@@ -40,11 +41,13 @@ export default async function Home() {
 
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">
-          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+          {session?.user
+            ? `Olá, ${session.user.name?.split(" ")[0]}!`
+            : "Hello! Let's schedule a haircut today?"}
         </h2>
         <p className="capitalize text-sm">
-          {format(new Date(), "EEEE',' dd 'de' MMMM", {
-            locale: ptBR,
+          {format(new Date(), "EEEE',' dd 'of' MMMM", {
+            locale: enCA,
           })}
         </p>
       </div>
@@ -56,7 +59,9 @@ export default async function Home() {
       <div className="mt-6">
         {confirmedBookings.length > 0 && (
           <>
-            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+              Appointments
+            </h2>
             <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
               {confirmedBookings.map((booking) => (
                 <BookingItem key={booking.id} booking={booking} />
@@ -67,7 +72,9 @@ export default async function Home() {
       </div>
 
       <div className="mt-6">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Recommended
+        </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
@@ -79,7 +86,9 @@ export default async function Home() {
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Popular
+        </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {recommendedBarbershops.map((barbershop) => (
